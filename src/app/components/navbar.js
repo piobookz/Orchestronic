8367ac5318f98c76737e2557dd96044350f8e9c7
PattiyaY <pattiya.2547@icloud.com>
2024-11-11 15:49:38 +0700

    feat: navbar V.1.1

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -4,6 +4,7 @@ import logo from "../../../public/idp-logo.png";
 import person from "../../../public/image.png";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
 export default function Navbar() {
   return (
@@ -11,31 +12,49 @@ export default function Navbar() {
       <div className="flex flex-row justify-between items-center">
         <div className="flex flex-row">
           <Link href={"/homepage"}>
-            <Image src={logo} width="60" height="60" className="mr-5"></Image>
+            <Image
+              src={logo}
+              width="70"
+              height="70"
+              alt="logo"
+              className="mr-5"
+            ></Image>
           </Link>
-          <ul className="flex flex-row space-x-10 text-center items-center">
-            <Link href={"/homepage"} className="">
+          <ul className="flex flex-row space-x-6 text-center items-center">
+            <Link
+              href="/homepage"
+              className="text-white hover:text-[#07032B] hover:bg-[rgba(255,255,255,0.85)] rounded px-2 py-1 transition duration-300"
+            >
               <li>Home</li>
             </Link>
-            <Link href={"/homepage"} className="">
+            <Link
+              href="/homepage"
+              className="text-white hover:text-[#07032B] hover:bg-[rgba(255,255,255,0.85)] rounded px-2 py-1 transition duration-300"
+            >
               <li>Project</li>
             </Link>
-            <Link href={"/homepage"} className="">
+            <Link
+              href="/homepage"
+              className="text-white hover:text-[#07032B] hover:bg-[rgba(255,255,255,0.85)] rounded px-2 py-1 transition duration-300"
+            >
               <li>Policy</li>
             </Link>
           </ul>
         </div>
-        <div className="flex flex-row">
-          <Image
-            src={person}
-            width="60"
-            height="60"
-            className="mr-5 rounded-full"
-          ></Image>
-          <div className="flex flex-col">
-            <h4>Alex</h4>
-            <p>Developer</p>
-          </div>
+        <div>
+          <Link href={"/homepage"} className="flex flex-row items-center">
+            <Image
+              src={person}
+              alt="profile"
+              className="h-10 w-10 rounded-full mr-4 focus:ring"
+            ></Image>
+            <div className="flex flex-col">
+              <h3>
+                <b>Alex</b>
+              </h3>
+              <p>Developer</p>
+            </div>
+          </Link>
         </div>
       </div>
     </nav>
---
+++ src/app/homepage/page.js
@@ -0,0 +1,9 @@
+import Navbar from "orchestronic/app/components/navbar";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+}
