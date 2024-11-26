0678dbf3d2c251be00f849bde628a9412e5f275b
YUMI YOSHIDA <yumi.yoshida.t@gmail.com>
2024-11-26 22:30:31 +0700

    feat: api policy GET

--- src/app/api/policy/route.js
+++ src/app/api/policy/route.js
@@ -16,3 +16,22 @@ export async function POST(req) {
 
     return NextResponse.json({ message: "Successful set policy" }, { status: 201 });
 }
+
+export async function GET(req) {
+    try {
+        // Connect to MongoDB
+        await connectMongoDB();
+
+        // Retrieve all policy documents
+        const policies = await Policy.findOne({});
+
+        // Respond with the retrieved policies
+        return NextResponse.json({ data: policies }, { status: 200 });
+        
+    } catch (error) {
+        console.error("Error retrieving policies:", error);
+
+        // Respond with an error message
+        return NextResponse.json({ error: "Failed to fetch policies" }, { status: 500 });
+    }
+}
