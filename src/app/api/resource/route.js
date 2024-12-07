927a53830b1c7c35f5eb04166081b6651924f7b7
PattiyaY <pattiya.2547@icloud.com>
2024-12-07 15:56:17 +0700

    feat: retrieve data (resource api)

--- src/app/api/resource/route.js
+++ src/app/api/resource/route.js
@@ -14,6 +14,7 @@ export async function POST(req) {
     adminPassword,
     vmSize,
     allocation,
+    projectID,
   } = await req.json();
   console.log(
     userID,
@@ -24,7 +25,8 @@ export async function POST(req) {
     adminUser,
     adminPassword,
     vmSize,
-    allocation
+    allocation,
+    projectID
   );
 
   // Connect to MongoDB
@@ -41,10 +43,30 @@ export async function POST(req) {
     username: adminUser,
     password: adminPassword,
     allocationip: allocation,
+    projectid: projectID,
   });
 
   return NextResponse.json(
     { message: "Successfully added resource" },
     { status: 201 }
   );
+}
+
+export async function GET() {
+  try {
+    await connectMongoDB();
+    console.log("Connected to MongoDB");
+
+    const resources = await Resource.find({});
+    console.log("Fetched resources:", resources);
+
+    return NextResponse.json(resources, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching resources:", error);
+
+    return NextResponse.json(
+      { message: "Error fetching resources", error: error.message },
+      { status: 500 }
+    );
+  }
 }
