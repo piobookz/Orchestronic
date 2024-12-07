205d377d5d3935cdeefabc638dedc775615d4f9c
piobookz <154525962+piobookz@users.noreply.github.com>
2024-12-07 15:40:58 +0700

    Update page.js

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -17,18 +17,9 @@ export default function Projects() {
           </h1>
           <Link
             type="button"
-            className="inline-flex items-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 flex-none"
-            href={"/projects/new"}
+            className="inline-flex flex-none items-center rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            href={"/create-project"}
           >
-            {/* <svg
-              className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-              data-slot="icon"
-            >
-              <path d="M2.695 14.762l-1.262 3.155a.5.5 0 0 0 .65.65l3.155-1.262a4 4 0 0 0 1.343-.886L17.5 5.501a2.121 2.121 0 0 0-3-3L3.58 13.419a4 4 0 0 0-.885 1.343Z" />
-            </svg> */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
