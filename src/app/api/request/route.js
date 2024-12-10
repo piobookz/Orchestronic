70a5d913103839e26cc673bb994d10682aa0159c
PattiyaY <pattiya.2547@icloud.com>
2024-12-10 19:11:46 +0700

    feat: create request api (post)

---
+++ src/app/api/request/route.js
@@ -0,0 +1,28 @@
+import { connectMongoDB } from "../../../../lib/mongodb";
+import Request from "../../../../models/request";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const resources = await req.json(); // Parse JSON array
+  console.log("Incoming Requests:", resources);
+
+  // Connect to MongoDB
+  await connectMongoDB();
+
+  // Save each resource
+  const requests = await Request.insertMany(
+    resources.map(({ id, name, type, userid, projectid, status }) => ({
+      requestid: id,
+      name,
+      type,
+      userid,
+      projectid,
+      status,
+    }))
+  );
+
+  return NextResponse.json(
+    { message: "Successfully added requests", requests },
+    { status: 201 }
+  );
+}
