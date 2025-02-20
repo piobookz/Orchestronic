cfc93c0f1b80ab1f56d9c6cd30159f5f63bd4625
PattiyaY <pattiya.2547@icloud.com>
2025-02-20 22:02:08 +0700

    chore: install react-hot-toast

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "orchestronic/app/components/navbar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
+        <Toaster position="top-right" />
         {children}
       </body>
     </html>
