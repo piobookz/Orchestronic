2583565171094a27b7b3ef58128a89580e2c0dde
PattiyaY <pattiya.2547@icloud.com>
2025-02-23 22:02:23 +0700

    refactor: greet user after login

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
-"use client";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return <div>Sign in to view this page</div>;
+  }
+
+  const user = await currentUser();
+
   return (
     <>
       <p className="mx-16 my-5 text-balance text-center text-5xl font-bold text-white">
-        Welcome Back, Alex!
+        Welcome Back, {user.firstName}!
       </p>
       <h1 className="mx-16 my-5 text-balance text-4xl font-bold text-white">
         Updates
