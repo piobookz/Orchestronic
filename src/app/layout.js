53d87c913915f2ff5553ad1272ceba22282e0d6e
PattiyaY <pattiya.2547@icloud.com>
2025-02-26 02:46:01 +0700

    feat: landing page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "../app/components/navbar";
 import { Toaster } from "react-hot-toast";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
+import HomePage from "../app/page.js";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,27 +25,27 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header>
-            <SignedOut>
-              <Navbar />
-              <p className="text-center text-5xl font-bold text-white mt-16">
-                Welcome to Orchestronic!
-              </p>
-              {/* Show login button only when the user is signed out */}
-              <div className="flex justify-center items-center w-full mt-20">
-                <SignInButton mode="modal" className="font-semibold text-xl bg-white text-blue-500 px-6 py-3 rounded-lg shadow-lg hover:bg-blue-500 hover:text-white transition duration-300" />
-              </div>
-            </SignedOut>
-            <SignedIn>
-              {/* Show UserButton and Navbar when the user is signed in */}
-              <Navbar />
-              <Toaster position="top-right" />
-              {children}
-            </SignedIn>
-          </header>
+          {/* Signed-In Users */}
+          <SignedIn>
+            <Navbar />
+            <Toaster position="top-right" />
+            {children}
+          </SignedIn>
+
+          {/* Signed-Out Users */}
+          <SignedOut>
+            <HomePage />
+          </SignedOut>
         </body>
       </html>
     </ClerkProvider>
