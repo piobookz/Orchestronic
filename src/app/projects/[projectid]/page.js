c9d19801139d9b0a12d654150fff1aa2a4dce595
PattiyaY <pattiya.2547@icloud.com>
2025-02-25 20:17:13 +0700

    feat: add dynamic routes for projects

---
+++ src/app/projects/[projectid]/page.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function projectdetails() {
+  return <div>Project Details</div>;
+}
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,5 +1,6 @@
+"use client";
+
 import Link from "next/link";
-import { Container } from "postcss";
 
 export default function Projects() {
   return (
