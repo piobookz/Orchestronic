2cd22da5c52fae9e7479552dc382289b13fd8db2
YUMI YOSHIDA <yumi.yoshida.t@gmail.com>
2025-02-27 07:07:37 +0700

    test: route project

--- src/app/project/create/page.js
+++ src/app/project/create/page.js
@@ -1,181 +1,313 @@
 "use client";
-
-import gitlab from "../../../../public/gitlab-logo-500.svg";
-import Image from "next/image";
 import Link from "next/link";
-import { Card, Typography } from "@material-tailwind/react";
-import React, { useEffect, useState, use } from "react";
+import { React, useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import gitlab from "../../../../public/gitlab-logo-500.svg";
 import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
 import { useProvider } from "../../components/ConText";
 
-export default function RequestResource() {
-  const [project, setProject] = useState(null);
-  const data = useProvider();
-  const [TABLE_ROWS_CR, setTableRowsCR] = useState([]);
+export default function CreateProject() {
+  const { setProjectData } = useProvider();
   const router = useRouter();
-  const projectName = data?.projectData?.projectName;
-  const projectDescription = data?.projectData?.projectDescription;
-  const pathWithNamespace = data?.projectData?.pathWithNamespace;
+  const { user } = useUser();
+  const [projectList, setProjectList] = useState([]);
+  const [createdProjects, setCreatedProjects] = useState([]);
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [branch, setBranch] = useState("");
+  const [rootPath, setRootPath] = useState("");
+  const [pathWithNamespace, setPathWithNamespace] = useState("");
 
   useEffect(() => {
-    console.log("projectDetails", data?.projectData);
-    console.log("projectName", projectName);
-    console.log("projectDescription", projectDescription);
-    console.log("pathWithNamespace", pathWithNamespace);
-    const fetchResources = async () => {
+    if (!user) return;
+
+    // Fetch already created projects
+    const fetchCreatedProjects = async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/resource", {
+        const res = await fetch(`/api/project?userId=${user.id}`, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
           },
         });
-
-        if (!res.ok) {
-          throw new Error(`Error: ${res.status} - ${res.statusText}`);
+        const result = await res.json();
+        if (Array.isArray(result)) {
+          setCreatedProjects(result);
+        } else {
+          console.error("Expected array, but received:", result);
+          setCreatedProjects([]);
         }
-
-        const data = await res.json();
-        const rows = data.map((element) => ({
-          id: element._id,
-          name: element.vmname,
-          type: element.type,
-          userid: element.userid,
-          projectid: element.projectid,
-          statuspm: "Pending",
-          statusops: "Pending",
-        }));
-        setTableRowsCR(rows);
       } catch (error) {
-        console.log("Failed to fetch resources:", error.message);
+        console.error("Error fetching created projects:", error);
       }
     };
 
-    fetchResources();
-  }, [project]);
+    fetchCreatedProjects();
 
-  const handleRequest = async () => {
-    toast.success("Request sent successfully");
+    if (!user.externalAccounts || user.externalAccounts.length === 0) {
+      console.log("No external accounts found.");
+      return;
+    }
 
+    const gitlabAccount = user.externalAccounts.find(
+      (account) => account.provider === "gitlab"
+    );
+
+    if (!gitlabAccount) {
+      console.log("No GitLab account linked.");
+      return;
+    }
+
+    const gitlabToken = user.externalAccounts?.[0]?.providerUserId;
+
+    if (!gitlabToken) {
+      console.log("GitLab token is missing.");
+      return;
+    }
+
+    const fetchProjects = async () => {
+      // console.log(user);
+      try {
+        const res = await fetch(
+          `https://gitlab.com/api/v4/users/${gitlabToken}/projects`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (!res.ok) {
+          throw new Error(`Failed to fetch projects: ${res.statusText}`);
+        }
+
+        const projects = await res.json();
+        setProjectList(projects);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    fetchProjects();
+  }, [user]);
+
+  // Filter out already created repositories
+  const filteredProjectList = projectList.filter(
+    (project) =>
+      !createdProjects.some(
+        (createdProject) =>
+          createdProject.pathWithNamespace === project.path_with_namespace
+      )
+  );
+
+  if (!user) {
+    return <div>Sign in to view this page</div>;
+  }
+
+  const handleProjectChange = (e) => {
+    const selectedRepo = e.target.value;
+    if (!selectedRepo) {
+      setProjectName("");
+      setProjectDescription("");
+      setBranch("");
+      setRootPath("");
+      setPathWithNamespace("");
+      return;
+    }
+
+    const project = projectList.find(
+      (project) => project.path_with_namespace === selectedRepo
+    );
+
+    if (project) {
+      setProjectName(project.name || "");
+      setProjectDescription(project.description || "");
+      setBranch(project.default_branch || "");
+      setRootPath(project.http_url_to_repo || "");
+      setPathWithNamespace(project.path_with_namespace || "");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const projectDetails = {
+      projectName,
+      projectDescription,
+      pathWithNamespace,
+      branch,
+      rootPath,
+      userId: user.id, // Include the user ID
+      statuspm: "Pending",
+      statusops: "Pending",
+    };
+    // console.log(projectDetails);
     try {
-      const res = await fetch("http://localhost:3000/api/request", {
+      const res = await fetch("/api/project", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(TABLE_ROWS_CR),
+        body: JSON.stringify(projectDetails),
       });
 
       if (!res.ok) {
-        throw new Error(`Error: ${res.status} - ${res.statusText}`);
-      } else {
-        router.push("/projectlist");
+        throw new Error("Failed to save project");
       }
+
+      setProjectData(projectDetails);
+      router.push(`/project/create/request`);
     } catch (error) {
-      console.log("Error while saving request:", error.message);
+      console.error("Error saving project:", error);
+      alert("Failed to save project. Please try again.");
     }
   };
 
-  if (!data.projectData) {
-    return <p>Loading project details...</p>;
-  }
-
   return (
-    <div>
-      <h1 className="text-5xl font-bold mx-16 my-5">Todo List</h1>
-      <div className="bg-white mx-16 my-8 py-8 text-black text-xl rounded-2xl font-normal">
-        <div className="flex flex-row justify-between items-center">
-          <h1 className="text-3xl font-semibold ml-4">Application Details</h1>
-          <button
-            className="mr-4 text-sm text-white bg-[#29B95F] rounded py-2 px-2"
-            onClick={handleRequest}
-          >
-            Send Request
-          </button>
-        </div>
-        <div className="grid grid-rows-1 grid-flow-col gap-3 items-top mt-10">
+    <>
+      <h1 className="mx-16 my-10 text-4xl font-bold dark:text-white">
+        Create a New Project
+      </h1>
+
+      <section className="mx-16 rounded-lg bg-white p-8 shadow-lg dark:bg-gray-800">
+        <h2 className="mb-4 text-xl font-semibold capitalize text-gray-700 dark:text-white">
+          General Information
+        </h2>
+
+        <form className="space-y-8" onSubmit={handleSubmit}>
+          {/* Git Repository Section */}
           <div>
-            <p className="text-xl font-medium mx-16 mt-5">Application name</p>
-            <p className="text-lg font-normal ml-16 mt-2">{projectName}</p>
-          </div>
-          <div>
-            <p className="text-xl font-medium mx-16 mt-5">Description</p>
-            <p className="text-lg font-normal ml-16 mt-2">
-              {projectDescription}
-            </p>
-          </div>
-          <div>
-            <p className="text-xl font-medium mx-16 mt-5">Repository</p>
-            <div className="flex flex-row mx-16">
-              <Image src={gitlab} width="45" height="45" alt="logo" />
-              <p className="text-lg font-normal flex items-center">
-                {pathWithNamespace}
-              </p>
+            <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2">
+              <div className="flex flex-col items-left space-x-4">
+                <label className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-200">
+                  Git Repository
+                </label>
+                <div className="inline-flex justify-left items-center">
+                  <Image
+                    src={gitlab}
+                    width="60"
+                    height="60"
+                    alt="GitLab logo"
+                  />
+                  <span className="text-black">GitLab</span>
+                </div>
+              </div>
+
+              {/* Repository Selection */}
+              <div>
+                <label
+                  htmlFor="repository"
+                  className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-200"
+                >
+                  Select Repository
+                </label>
+                <select
+                  id="repository"
+                  className="w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-gray-700 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-400 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300"
+                  onChange={handleProjectChange}
+                >
+                  <option value="">Please select</option>
+                  {filteredProjectList.map((project) => (
+                    <option
+                      key={project.id}
+                      value={project.path_with_namespace}
+                    >
+                      {project.path_with_namespace}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="grid grid-rows-[auto,auto] grid-flow-col mt-10">
-          <div className="flex flex-row items-center h-12">
-            <p className="text-xl font-medium ml-16 mr-5 mt-5">
-              Cloud Resources
-            </p>
-            <Link href="/cloudresources">
-              <button className="mr-4 mt-5 text-sm text-white bg-[#0A7AFF] rounded py-1 px-8">
-                Add
-              </button>
-            </Link>
+          {/* Branch and Root Application Path Section */}
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+            <div>
+              <label
+                htmlFor="branch"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+              >
+                Default Branch
+              </label>
+              <input
+                id="branch"
+                type="text"
+                value={branch}
+                readOnly
+                className="mt-2 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-gray-700 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-400 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="rootPath"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+              >
+                Root Application Path
+              </label>
+              <input
+                id="rootPath"
+                type="text"
+                value={rootPath}
+                readOnly
+                className="mt-2 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-gray-700 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-400 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300"
+              />
+            </div>
           </div>
 
-          <Card className="overflow-hidden rounded-lg shadow-lg mx-16 mt-8">
-            <table className="w-full min-w-max table-auto text-left">
-              <thead>
-                <tr>
-                  <th className="border-b border-blue-gray-100 bg-gray-100 p-4 text-black font-semibold">
-                    Name
-                  </th>
-                  <th className="border-b border-blue-gray-100 bg-gray-100 p-4 text-black font-semibold">
-                    Type
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {TABLE_ROWS_CR.map(({ id, name, type }, index) => {
-                  const isOdd = index % 2 === 1;
-                  const rowBgColor = isOdd ? "bg-gray-50" : "bg-white";
-                  return (
-                    <tr key={id} className={`${rowBgColor} cursor-pointer`}>
-                      <td className="p-4 border-b border-blue-gray-50">
-                        <Link href={`/requestresource/${id}`}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {name}
-                          </Typography>
-                        </Link>
-                      </td>
-                      <td className="p-4 border-b border-blue-gray-50">
-                        <Link href={`/requestresource/${id}`}>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {type}
-                          </Typography>
-                        </Link>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </Card>
-        </div>
-      </div>
-    </div>
+          {/* Application Details Section */}
+          <div>
+            <label
+              htmlFor="applicationName"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+            >
+              Application Name
+            </label>
+            <input
+              id="applicationName"
+              type="text"
+              value={projectName}
+              readOnly
+              className="mt-2 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-gray-700 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-400 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              value={projectDescription}
+              onChange={(e) => setProjectDescription(e.target.value)} // Add this line
+              className="mt-2 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-gray-700 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-400 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300"
+              rows="4"
+            ></textarea>
+          </div>
+
+          {/* Buttons */}
+          <div className="flex justify-end space-x-4">
+            <button
+              type="button"
+              className="rounded-md bg-gray-200 px-7 py-2 text-sm font-medium text-black transition hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-200 dark:hover:bg-gray-500"
+              onClick={() => router.back()}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-green-500 px-8 py-2 text-sm font-medium text-white transition hover:bg-green-600 focus:outline-none focus:ring focus:ring-green-400"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
---
+++ src/app/project/create/request/page.js
@@ -0,0 +1,183 @@
+"use client";
+
+import gitlab from "../../../../../public/gitlab-logo-500.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { Card, Typography } from "@material-tailwind/react";
+import React, { useEffect, useState, use } from "react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { useProvider } from "../../../components/ConText";
+import { useAuth } from "@clerk/nextjs";
+
+export default function RequestResource() {
+  const { userId } = useAuth();
+  const [project, setProject] = useState(null);
+  const data = useProvider();
+  const [TABLE_ROWS_CR, setTableRowsCR] = useState([]);
+  const router = useRouter();
+  const projectName = data?.projectData?.projectName;
+  const projectDescription = data?.projectData?.projectDescription;
+  const pathWithNamespace = data?.projectData?.pathWithNamespace;
+
+  useEffect(() => {
+    console.log("projectDetails", data?.projectData);
+    console.log("projectName", projectName);
+    console.log("projectDescription", projectDescription);
+    console.log("pathWithNamespace", pathWithNamespace);
+    const fetchResources = async () => {
+      try {
+        const res = await fetch(`/api/resource?userId=${userId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error(`Error: ${res.status} - ${res.statusText}`);
+        }
+
+        const data = await res.json();
+        const rows = data.map((element) => ({
+          id: element._id,
+          name: element.vmname,
+          type: element.type,
+          userid: element.userid,
+          projectid: element.projectid,
+          statuspm: "Pending",
+          statusops: "Pending",
+        }));
+        setTableRowsCR(rows);
+      } catch (error) {
+        console.log("Failed to fetch resources:", error.message);
+      }
+    };
+
+    fetchResources();
+  }, [project]);
+
+  const handleRequest = async () => {
+    toast.success("Request sent successfully");
+
+    try {
+      const res = await fetch("http://localhost:3000/api/request", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(TABLE_ROWS_CR),
+      });
+
+      if (!res.ok) {
+        throw new Error(`Error: ${res.status} - ${res.statusText}`);
+      } else {
+        router.push("/project");
+      }
+    } catch (error) {
+      console.log("Error while saving request:", error.message);
+    }
+  };
+
+  if (!data.projectData) {
+    return <p>Loading project details...</p>;
+  }
+
+  return (
+    <div>
+      <h1 className="text-5xl font-bold mx-16 my-5">Todo List</h1>
+      <div className="bg-white mx-16 my-8 py-8 text-black text-xl rounded-2xl font-normal">
+        <div className="flex flex-row justify-between items-center">
+          <h1 className="text-3xl font-semibold ml-4">Application Details</h1>
+          <button
+            className="mr-4 text-sm text-white bg-[#29B95F] rounded py-2 px-2"
+            onClick={handleRequest}
+          >
+            Send Request
+          </button>
+        </div>
+        <div className="grid grid-rows-1 grid-flow-col gap-3 items-top mt-10">
+          <div>
+            <p className="text-xl font-medium mx-16 mt-5">Application name</p>
+            <p className="text-lg font-normal ml-16 mt-2">{projectName}</p>
+          </div>
+          <div>
+            <p className="text-xl font-medium mx-16 mt-5">Description</p>
+            <p className="text-lg font-normal ml-16 mt-2">
+              {projectDescription}
+            </p>
+          </div>
+          <div>
+            <p className="text-xl font-medium mx-16 mt-5">Repository</p>
+            <div className="flex flex-row mx-16">
+              <Image src={gitlab} width="45" height="45" alt="logo" />
+              <p className="text-lg font-normal flex items-center">
+                {pathWithNamespace}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-rows-[auto,auto] grid-flow-col mt-10">
+          <div className="flex flex-row items-center h-12">
+            <p className="text-xl font-medium ml-16 mr-5 mt-5">
+              Cloud Resources
+            </p>
+            <Link href="/cloudresources">
+              <button className="mr-4 mt-5 text-sm text-white bg-[#0A7AFF] rounded py-1 px-8">
+                Add
+              </button>
+            </Link>
+          </div>
+
+          <Card className="overflow-hidden rounded-lg shadow-lg mx-16 mt-8">
+            <table className="w-full min-w-max table-auto text-left">
+              <thead>
+                <tr>
+                  <th className="border-b border-blue-gray-100 bg-gray-100 p-4 text-black font-semibold">
+                    Name
+                  </th>
+                  <th className="border-b border-blue-gray-100 bg-gray-100 p-4 text-black font-semibold">
+                    Type
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {TABLE_ROWS_CR.map(({ id, name, type }, index) => {
+                  const isOdd = index % 2 === 1;
+                  const rowBgColor = isOdd ? "bg-gray-50" : "bg-white";
+                  return (
+                    <tr key={id} className={`${rowBgColor} cursor-pointer`}>
+                      <td className="p-4 border-b border-blue-gray-50">
+                        <Link href={`/requestresource/${id}`}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {name}
+                          </Typography>
+                        </Link>
+                      </td>
+                      <td className="p-4 border-b border-blue-gray-50">
+                        <Link href={`/requestresource/${id}`}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {type}
+                          </Typography>
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
